eaf6fe193d6f2ea92ad1bf51125c02779990638f
manor hagage <manorhagage@gmail.com>
2021-01-20 17:33:14 +0200

    general style

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 function App() {
   return (
     <div className="App">
-      
+      <span className="background"> </span>
+      <h1> Hello Manor! </h1>
+      <p className="tasks-counter"> you have 0 tasks todo.</p>
+      <button className="btn">Create New Folder </button>
+      <button className="btn-clean"> edit </button>
     </div>
   );
 }
